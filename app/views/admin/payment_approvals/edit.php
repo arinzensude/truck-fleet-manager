2effe53a1b2966bbdd97d9fd84e9b1f6efc14bde
Arinze Nsude <arinzensude@nadlon.co>
2021-02-10 03:18:30 +0100

    Payment Approval Edit: make all the fields non-editable for admin. Increased the width of the Truck textbox too

--- app/views/admin/payment_approvals/edit.php
+++ app/views/admin/payment_approvals/edit.php
@@ -2,17 +2,17 @@
 
 <?php echo $this->form->create($model->name, array('is_admin' => $this->is_admin)); ?>
 <?php echo $this->form->open_admin_table(); ?>
-<?php if($object->approved) {?>
-	<?php echo $this->form->belongs_to_dropdown('Truck', $trucks, array('style' => 'width: 100px;', 'empty' => true, 'readonly'=>'readonly')); ?>
+<?php if($object->approved || current_user_can('administrator')) {?>
+	<?php echo $this->form->belongs_to_dropdown('Truck', $trucks, array('style' => 'width: 300px;', 'empty' => true, 'disabled'=>'true')); ?>
 	<?php echo $this->form->input('amount', array('readonly'=>'readonly')); ?>
 	<?php echo $this->form->textarea_input('description', array('label' => 'Description', 'readonly'=>'readonly')); ?>
 <?php } else {?>
-	<?php echo $this->form->belongs_to_dropdown('Truck', $trucks, array('style' => 'width: 100px;', 'empty' => true)); ?>
+	<?php echo $this->form->belongs_to_dropdown('Truck', $trucks, array('style' => 'width: 300px;', 'empty' => true)); ?>
 	<?php echo $this->form->input('amount'); ?>
 	<?php echo $this->form->textarea_input('description', array('label' => 'Description')); ?>
-	<?php if(current_user_can('administrator')) {?>
-		<?php echo $this->form->checkbox_input('approved', array('label' => 'Approved')); ?>
-	<?php }?>
+<?php }?>
+<?php if(!$object->approved && current_user_can('administrator')) {?>
+	<?php echo $this->form->checkbox_input('approved', array('label' => 'Approved')); ?>
 <?php }?>
 <?php echo $this->form->close_admin_table(); ?>
 <?php echo $this->form->end('Update'); ?>
